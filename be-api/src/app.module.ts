74e5eaf4bf46d82de8695eb52fa4b13f1034eae8
Krystian Olbrys <x11.crs.539@gmail.com>
2022-02-18 13:16:21 +0100

    CleanUp

--- be-api/src/app.module.ts
+++ be-api/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PredictionService } from './V1/BackEnd/Services/Implementations/prediction.service';
 import { PredictionsController } from './V1/Controllers/predictions.controller';
 
 @Module({
   imports: [],
   controllers: [AppController, PredictionsController],
-  providers: [AppService, PredictionService],
+  providers: [PredictionService],
 })
 export class AppModule { }
